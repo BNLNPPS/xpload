4f392cd9dc804a22d6504a35629f216801b53ee9
Dmitri Smirnov <dmixsmi@gmail.com>
2021-10-03 09:57:59 -0400

    Move implementation from fetch.h to fetch.cpp

--- include/xpload/fetch.h
+++ include/xpload/fetch.h
@@ -1,46 +1,12 @@
 #pragma once
 
 #include <cstdint>
-#include <optional>
-
-#include <curl/curl.h>
-#include <json/json.h>
+#include <string>
+#include <vector>
 
 
 namespace xpload {
 
-std::optional<std::string> fetch(uint64_t timestamp)
-{
-  curl_version_info_data *curlver_data = curl_version_info(CURLVERSION_NOW);
-  std::string useragent{"curl/" + std::string(curlver_data->version)};
-
-  CURL *curl;
-  CURLcode result;
-
-  curl_global_init(CURL_GLOBAL_ALL);
-  curl = curl_easy_init();
-
-  if (curl) {
-    // First set the URL that is about to receive our POST. This URL can just as
-    // well be a https:// URL if that is what should receive the data.
-    curl_easy_setopt(curl, CURLOPT_URL, "http://postit.example.com/moo.cgi");
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, useragent.c_str());
-
-    // Now specify the POST data
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
-
-    // Perform the request, result will get the return code
-    result = curl_easy_perform(curl);
-
-    if (result != CURLE_OK)
-      std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << '\n';
-
-    curl_easy_cleanup(curl);
-  }
-
-  curl_global_cleanup();
-
-  return {};
-}
+std::vector<std::string> fetch(std::string tag, uint64_t timestamp);
 
 }
