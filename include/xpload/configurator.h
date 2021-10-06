800d1c393ac674ff3e9f0f83dbe0cb059fca12b1
Dmitri Smirnov <dmixsmi@gmail.com>
2021-10-06 19:38:46 -0400

    Add xpload config to Configurator

--- include/xpload/configurator.h
+++ include/xpload/configurator.h
@@ -4,6 +4,17 @@
 
 namespace xpload {
 
+struct DbConfig
+{
+  std::string host{"localhost"};
+  std::string apiroot{"/api/cdb_rest"};
+  std::string port{"8000"};
+  std::string path{"/path/to/payload/data"};
+
+  std::string url() const { return "http://" + host + ':' + port + apiroot; }
+};
+
+
 /**
  * Provides access to all configuration types and data.
  */
@@ -16,6 +27,7 @@ class Configurator
   /// A unique name associated with this Configurator
   std::string name;
 
+  DbConfig db;
 };
 
 }
