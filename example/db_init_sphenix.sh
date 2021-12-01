75bdfb4b57e4c8802e3fdabef81d7d44dbdaafb2
Dmitri Smirnov <dmixsmi@gmail.com>
2021-12-01 17:09:35 -0500

    example: Add DB init script for sPHENIX experiment

---
+++ example/db_init_sphenix.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+xpl push example_tag_1 Beam     D0DXMagnets.dat
+xpl push example_tag_1 MagField sphenix3dbigmapxyz.root
+xpl push example_tag_1 CEMC     CEMCprof_Thresh30MeV.root
+xpl push example_tag_1 ZDC      towerMap_ZDC.txt
+xpl push example_tag_1 CEMC     cemc_geoparams-0-0-4294967295-1536789215.xml
