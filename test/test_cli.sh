8cf80086ec6b84b016547ea4ee30035b2bac0ec0
Dmitri Smirnov <dmixsmi@gmail.com>
2022-03-28 19:12:50 -0400

    test: Update for multiple file storage prefixes

--- test/test_cli.sh
+++ test/test_cli.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=`dirname "$0"`
-DATA_DIR=`xpl config path`
-mkdir -p $DATA_DIR
 
-echo $SCRIPT_DIR
+DATA_DIR_0=`xpl config path 0`
+DATA_DIR_1=`xpl config path 1`
+DATA_DIR_N=/path/not/from/config
 
-dd if=/dev/random of=$DATA_DIR/payload_10.data bs=1k count=1 &> /dev/null
-dd if=/dev/random of=$DATA_DIR/payload_11.data bs=1k count=1 &> /dev/null
+rm -fr   $DATA_DIR_0 $DATA_DIR_1 $DATA_DIR_N
+mkdir -p $DATA_DIR_0 $DATA_DIR_1 $DATA_DIR_N
 
-ls -la $DATA_DIR/
+dd if=/dev/random of=$DATA_DIR_0/payload_00.data bs=1k count=1 &> /dev/null
+dd if=/dev/random of=$DATA_DIR_0/payload_01.data bs=1k count=1 &> /dev/null
+dd if=/dev/random of=$DATA_DIR_1/payload_10.data bs=1k count=1 &> /dev/null
+dd if=/dev/random of=$DATA_DIR_1/payload_11.data bs=1k count=1 &> /dev/null
+dd if=/dev/random of=$DATA_DIR_N/payload_N0.data bs=1k count=1 &> /dev/null
 
-md5sum $DATA_DIR/payload_10.data
-md5sum $DATA_DIR/payload_11.data
+md5sum $DATA_DIR_0/payload_00.data
+md5sum $DATA_DIR_0/payload_01.data
+md5sum $DATA_DIR_1/payload_10.data
+md5sum $DATA_DIR_1/payload_11.data
+md5sum $DATA_DIR_N/payload_N0.data
 
-xpl insert tag_1 domain_1 payload_10.data -s 3
-xpl insert tag_1 domain_2 payload_11.data -s 5
+xpl insert tag_1 domain_1 payload_00.data -s 3
+xpl insert tag_1 domain_2 payload_01.data -s 5
 xpl insert tag_2 domain_1 payload_10.data -s 7
 xpl insert tag_2 domain_2 payload_11.data -s 9
+xpl insert tag_3 domain_1 payload_N0.data -s 1
 
 rm -f /tmp/test_cli.out
 
@@ -31,6 +39,11 @@ xpl fetch tag_1 -s 3
 xpl fetch tag_1 -s 5
 xpl fetch tag_1 -s 3 -d domain_1
 xpl fetch tag_1 -s 3 -d domain_2
+
+xpl fetch tag_2 -d domain_1
+xpl fetch tag_2 -d domain_2
+
+xpl fetch tag_3 -d domain_1
 } |& tee -a /tmp/test_cli.out
 
 
