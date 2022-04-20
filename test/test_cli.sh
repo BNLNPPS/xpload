7042fd60a424c3402fea97b0608d6c4e4903b7b2
Dmitri Smirnov <dmixsmi@gmail.com>
2022-04-19 20:18:32 -0400

    test: Switch from insert to add/push actions

--- test/test_cli.sh
+++ test/test_cli.sh
@@ -4,32 +4,31 @@ SCRIPT_DIR=`dirname "$0"`
 
 DATA_DIR_0=`xpl config path 0`
 DATA_DIR_1=`xpl config path 1`
-DATA_DIR_N=/tmp/path/not/from/config
 
-rm -fr   $DATA_DIR_0 $DATA_DIR_1 $DATA_DIR_N
-mkdir -p $DATA_DIR_0 $DATA_DIR_1 $DATA_DIR_N
+rm -fr   $DATA_DIR_0 $DATA_DIR_1
+mkdir -p $DATA_DIR_1
 
-dd if=/dev/random of=$DATA_DIR_0/payload_00.data bs=1k count=1 &> /dev/null
-dd if=/dev/random of=$DATA_DIR_0/payload_01.data bs=1k count=1 &> /dev/null
-dd if=/dev/random of=$DATA_DIR_1/payload_10.data bs=1k count=1 &> /dev/null
-dd if=/dev/random of=$DATA_DIR_1/payload_11.data bs=1k count=1 &> /dev/null
-dd if=/dev/random of=$DATA_DIR_N/payload_N0.data bs=1k count=1 &> /dev/null
+randfile() { python -c 'import random, sys; random.seed(int(sys.argv[1])); sys.stdout.buffer.write(random.randbytes(1000))' "$@"; }
 
-md5sum $DATA_DIR_0/payload_00.data
-md5sum $DATA_DIR_0/payload_01.data
-md5sum $DATA_DIR_1/payload_10.data
-md5sum $DATA_DIR_1/payload_11.data
-md5sum $DATA_DIR_N/payload_N0.data
-
-xpl insert tag_1 domain_1 payload_00.data -s 3
-xpl insert tag_1 domain_2 payload_01.data -s 5
-xpl insert tag_2 domain_1 payload_10.data -s 7
-xpl insert tag_2 domain_2 payload_11.data -s 9
-xpl insert tag_3 domain_1 payload_N0.data -s 1
+randfile 1 > /tmp/payload_00.data
+randfile 2 > /tmp/payload_01.data
+randfile 3 > /tmp/payload_10.data
+randfile 4 > /tmp/payload_11.data
+randfile 5 > /tmp/payload_N0.data
 
 rm -f /tmp/test_cli.out
 
 {
+xpl add tag tag_1 -t tag_1_type -s tag_1_status -d domain_1 domain_2
+xpl add tag tag_2 -t tag_2_type -s tag_2_status -d domain_1 domain_2
+xpl add tag tag_3 -t tag_3_type -s tag_3_status -d domain_3
+xpl add pil tag_1 domain_1 /tmp/payload_00.data -s 3
+xpl add pil tag_1 domain_2 /tmp/payload_01.data -s 5
+xpl add pil tag_2 domain_1 /tmp/payload_10.data -s 7
+xpl add pil tag_2 domain_2 /tmp/payload_11.data -s 9
+xpl add pil tag_3 domain_3 /tmp/payload_N0.data -s 1
+xpl push
+
 xpl show tags
 xpl show domains
 
