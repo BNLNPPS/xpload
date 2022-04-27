b7670fa08c7770c7c011359b16152601de51c336
Dmitri Smirnov <dmixsmi@gmail.com>
2022-04-27 10:36:21 -0400

    fetch: Fix clang warning about -Wrange-loop-construct
    ```
    cd /build/test && /usr/bin/clang++  -I/xpload/include -Wall -Wextra -Wpedantic -std=c++17 -MD -MT test/CMakeFiles/test_xpload.dir/test_xpload.cpp.o -MF CMakeFiles/test_xpload.dir/test_xpload.cpp.o.d -o CMakeFiles/test_xpload.dir/test_xpload.cpp.o -c /xpload/test/test_xpload.cpp
    /xpload/test/test_xpload.cpp:42:22: warning: loop variable 'path' of type 'const std::string &' (aka 'const basic_string<char> &') binds to a temporary constructed from type '__gnu_cxx::__normal_iterator<std::filesystem::path *, std::vector<std::filesystem::path>>::reference' (aka 'std::filesystem::path &') [-Wrange-loop-construct]
      for (const string& path : result.paths)
                         ^
    /xpload/test/test_xpload.cpp:42:8: note: use non-reference type 'std::string' (aka 'basic_string<char>') to make construction explicit or type 'const std::filesystem::path &' to prevent copying
      for (const string& path : result.paths)
           ^~~~~~~~~~~~~~~~~~~~
    1 warning generated.
    ```

--- test/test_xpload.cpp
+++ test/test_xpload.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     return EXIT_FAILURE;
   }
 
-  for (const string& path : result.paths)
+  for (const string path : result.paths)
     cout << path << '\n';
 
   return EXIT_SUCCESS;
