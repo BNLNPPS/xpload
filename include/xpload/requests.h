bc73bb20bbb2a61fb7018ca664f620a7045a6cf6
Dmitri Smirnov <dmixsmi@gmail.com>
2022-09-27 14:07:03 -0400

    requests: Move save_data from fetch for future reuse

---
+++ include/xpload/requests.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+namespace xpload {
+
+/**
+ * Appends `chunk_size`*`n_chunks` characters from the `input` buffer to the
+ * output string `out`
+ *
+ * Complies with the signature of a "write function" as defined by libcurl:
+ *
+ *     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_data);
+ */
+std::size_t save_data(const char* input, std::size_t chunk_size, std::size_t n_chunks, std::string* out);
+
+}
